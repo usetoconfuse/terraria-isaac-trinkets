45b036e957029e88fc9a14035c51f30b580a8a21
usetoconfuse <toby.tischler@gmail.com>
2025-07-10 16:07:12 +0100

    Fix comment referencing TutorialMod

--- Content/Items/TutorialSword.cs
+++ Content/Items/TutorialSword.cs
@@ -9,7 +9,7 @@ namespace EpicestJamMod.Content.Items
 	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
 	public class TutorialSword : ModItem
 	{
-		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TutorialMod.hjson' file.
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.EpicestJamMod.hjson' file.
 		public override void SetDefaults()
 		{
 			Item.damage = 100;
