38eb94f4b3d3e4f0e9e024e899626f3a1bc60700
Dan Kerry <danieljameskerry@gmail.com>
2025-07-11 21:04:42 +0100

    changed slot position

--- UI/TrinketSlot.cs
+++ UI/TrinketSlot.cs
@@ -25,6 +25,8 @@ namespace IsaacTrinkets.UI
 		
 		public static LocalizedText TrinketSlotText { get; private set; }
 
+		public override Vector2? CustomLocation => new Vector2(4 * Main.EquipPage / 5, 3 * Main.screenHeight / 4);
+
 		public override bool DrawVanitySlot => false;
 		public override bool DrawDyeSlot => false;
 		
