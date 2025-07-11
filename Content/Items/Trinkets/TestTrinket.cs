dd6f73b8dd68cc16b6853e6db444bc7f33b33403
Dan Kerry <danieljameskerry@gmail.com>
2025-07-11 19:54:42 +0100

    abstract trinket class to extend from

--- Content/Items/Trinkets/TestTrinket.cs
+++ Content/Items/Trinkets/TestTrinket.cs
@@ -9,16 +9,13 @@ namespace IsaacTrinkets.Content.Items.Trinkets
 	// This is a basic item template.
 	// Please see tModLoader's ExampleMod for every other example:
 	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
-	public class TestTrinket : ModItem
+	public class TestTrinket : Trinket
 	{
 		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.IsaacTrinkets.hjson' file.
 		public override void SetDefaults()
 		{
-			Item.width = 40;
-			Item.height = 40;
-			Item.accessory = true;
-			Item.value = Item.buyPrice(silver: 1);
-			Item.rare = ItemRarityID.Blue;
+			base.SetDefaults();
+			Item.defense = 10;
 		}
 
 		public override void AddRecipes()
@@ -27,24 +24,6 @@ namespace IsaacTrinkets.Content.Items.Trinkets
 			recipe.AddIngredient(ItemID.DirtBlock, 10);
 			recipe.AddTile(TileID.WorkBenches);
 			recipe.Register();
-		}
-
-		public override bool CanEquipAccessory(Player player, int slot, bool modded)
-		{
-			if (modded)
-			{
-				int moddedSlot = LoaderManager.Get<AccessorySlotLoader>().Get(ModContent.GetInstance<TrinketSlot>().Type, player).Type;
-
-				//Main.NewText("Modded slot: " + moddedSlot.ToString() + ", Selected slot: " + slot.ToString(), 255, 255, 255);
-
-				if (moddedSlot == slot)
-				{
-					return true;
-				}
-			}
-			
-
-			return false;
 		}
 	}
 }
---
+++ Content/Items/Trinkets/Trinket.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using IsaacTrinkets.UI;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace IsaacTrinkets.Content.Items.Trinkets
+{ 
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+	public abstract class Trinket : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.IsaacTrinkets.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.width = 40;
+			Item.height = 40;
+			Item.accessory = true;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+		}
+
+		public override bool CanEquipAccessory(Player player, int slot, bool modded)
+		{
+			if (modded)
+			{
+				int moddedSlot = LoaderManager.Get<AccessorySlotLoader>().Get(ModContent.GetInstance<TrinketSlot>().Type, player).Type;
+
+				//Main.NewText("Modded slot: " + moddedSlot.ToString() + ", Selected slot: " + slot.ToString(), 255, 255, 255);
+
+				if (moddedSlot == slot)
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
+}
