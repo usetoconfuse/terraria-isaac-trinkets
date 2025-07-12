7da0466f2be7bd6581841e4b6d67cdd1db98a61a
Dan Kerry <danieljameskerry@gmail.com>
2025-07-12 13:52:59 +0100

    dim bulb implemented, vibrant bulb tooltip tweak

---
+++ Content/Items/Trinkets/DimBulb.cs
@@ -0,0 +1,33 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace IsaacTrinkets.Content.Items.Trinkets
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class DimBulb : TrinketItem
+    {
+        // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.IsaacTrinkets.hjson' file.
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (player.statLife < player.statLifeMax2 * 0.25f)
+            {
+                player.statDefense += 8;
+                player.GetDamage(DamageClass.Generic) += 0.08f;
+                player.GetCritChance(DamageClass.Generic) += 8;
+                player.equipmentBasedLuckBonus += 0.1f;
+
+                player.moveSpeed += 0.1f;
+            }
+        }
+    }
+}
