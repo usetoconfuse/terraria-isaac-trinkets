1511fb8f7f641dc4e40912e7d5a38a7bfdc0695f
Dan Kerry <danieljameskerry@gmail.com>
2026-07-03 19:54:55 +0100

    Added Karma

--- Content/Items/Trinkets/Karma.cs
+++ Content/Items/Trinkets/Karma.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Linq;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace IsaacTrinkets.Content.Items.Trinkets
 {
-    public class LuckyToe : TrinketItem
+    public class Karma : TrinketItem
     {
         public override void SetDefaults()
         {
@@ -14,7 +17,17 @@ namespace IsaacTrinkets.Content.Items.Trinkets
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.equipmentBasedLuckBonus += 0.2f;
+            //vanilla reasonable cap with all npcs: 300-400 (11 per npc) = 0.3-0.4 luck
+            int karma = 0;
+            foreach (var npc in Main.ActiveNPCs)
+            {
+                if (npc.townNPC)
+                {
+                    var price = Main.ShopHelper.GetShoppingSettings(player, npc).PriceAdjustment;
+                    karma += (int)((1 - price) * 100);
+                }
+            }
+            player.equipmentBasedLuckBonus += (float)karma / 1000;
         }
         public override void AddRecipes()
         {
