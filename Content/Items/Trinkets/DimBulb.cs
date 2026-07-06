d8840ec2e28bd2bf865cd6cc4a4f561e7127df45
Dan Kerry <danieljameskerry@gmail.com>
2026-07-06 23:40:37 +0100

    DimBulb and VibrantBulb can be shimmered between

--- Content/Items/Trinkets/DimBulb.cs
+++ Content/Items/Trinkets/DimBulb.cs
@@ -1,6 +1,7 @@
 using IsaacTrinkets.Common;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace IsaacTrinkets.Content.Items.Trinkets
 {
@@ -15,18 +16,12 @@ namespace IsaacTrinkets.Content.Items.Trinkets
             base.SetDefaults();
             Item.value = Item.buyPrice(silver: 1);
             Item.rare = ItemRarityID.Blue;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<VibrantBulb>();
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetModPlayer<TrinketPlayer>().dimBulbAcc = true;
-        }
-        public override void AddRecipes()
-        {
-            Recipe recipe = CreateRecipe();
-            recipe.AddRecipeGroup(RecipeGroupID.IronBar, 10);
-            recipe.AddTile(TileID.TinkerersWorkbench);
-            recipe.Register();
         }
     }
 }
--- Content/Items/Trinkets/VibrantBulb.cs
+++ Content/Items/Trinkets/VibrantBulb.cs
@@ -1,6 +1,7 @@
 using IsaacTrinkets.Common;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace IsaacTrinkets.Content.Items.Trinkets
 {
@@ -15,19 +16,12 @@ namespace IsaacTrinkets.Content.Items.Trinkets
             base.SetDefaults();
             Item.value = Item.buyPrice(silver: 1);
             Item.rare = ItemRarityID.Blue;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<DimBulb>();
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetModPlayer<TrinketPlayer>().vibrantBulbAcc = true;
-        }
-
-        public override void AddRecipes()
-        {
-            Recipe recipe = CreateRecipe();
-            recipe.AddRecipeGroup(RecipeGroupID.IronBar, 10);
-            recipe.AddTile(TileID.TinkerersWorkbench);
-            recipe.Register();
         }
     }
 }
